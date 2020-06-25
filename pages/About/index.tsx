75ae9ee7ea4ebfe07ba2fb09811fd4ef26f7dd51
João Lourenço <jrelourenco@outlook.pt>
2020-06-25 14:03:13 +0000

    chore: Init project

---
+++ pages/About/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Page: React.FC = () => {
+  return <div>About</div>
+}
+
+export default Page
---
+++ pages/Home/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Page: React.FC = () => {
+  return <div>Welcome to Next.js!</div>
+}
+
+export default Page
