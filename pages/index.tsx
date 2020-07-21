01aeade67e1d013919e88bcdc19c1a6c6613bacf
João Lourenço <jrelourenco@outlook.pt>
2020-07-21 17:43:10 +0000

    fix: Implemet lighthouse fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import Home from './Home'
+import Head from 'next/head'
 
 const Page: React.FC = () => {
-  return <Home />
+  return (
+    <div>
+      <Head>
+        <title>Jrel Shortener</title>
+        <meta name="description" content="Jrel Shortener "></meta>
+      </Head>
+      <Home />
+    </div>
+  )
 }
 
 export default Page
